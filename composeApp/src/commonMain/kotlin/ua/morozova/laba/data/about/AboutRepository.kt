a0d24e4b21c478f8db7af34ee23786b824109c6a
AnnaMorozovac <morozova.anna@chnu.edu.ua>
2026-05-07 17:46:08 +0300

    [6] Implement AboutRepository for system information (laba8)

--- composeApp/src/commonMain/kotlin/ua/morozova/laba/data/about/AboutRepository.kt
+++ composeApp/src/commonMain/kotlin/ua/morozova/laba/data/about/AboutRepository.kt
@@ -3,10 +3,10 @@ package ua.morozova.laba.data.about
 import kotlin.math.max
 import kotlin.math.min
 
-internal class AboutRepository {
-
+internal class AboutRepository(
+    private val platform: Platform
+) {
     fun getAbout(): MutableList<Pair<String, String>> {
-        val platform = Platform()
 
         val items = mutableListOf(
             Pair("Operating System", "${platform.osName} ${platform.osVersion}"),
